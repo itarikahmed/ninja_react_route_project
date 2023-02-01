96904649f2c4f9c4e189ce29b65859819515fc1e
Tarik-Ahmed75 <tarik.ahmed@northsouth.edu>
2023-02-02 02:57:28 +0600

    pages and layout done

--- ninja_react_router_project/src/component/App.jsx
+++ ninja_react_router_project/src/component/App.jsx
@@ -1,9 +1,55 @@
-function App() {
+/**
+ * previously used react router
+ * 
+ * import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
+ * 
+ function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>JobRouter</h1>
+          <Link to="/">Home</Link>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
+}
+ */
+
+//pages
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+//Layouts
+
+import RootLayout from "../Layouts/RootLayout";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
